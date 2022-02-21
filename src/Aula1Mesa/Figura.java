f2676760b53a576c925e89d2e321ed2008732ab9
marcia yurie yano <83242742+marciayano@users.noreply.github.com>
2022-02-21 11:48:46 -0300

    Initial commit

---
+++ src/Aula1Mesa/Figura.java
@@ -0,0 +1,12 @@
+package Aula1Mesa;
+//Crie uma classe Figura e duas subclasses de Figura.
+// Uma deve ser chamada de círculo e a segunda de quadrado.
+// Para cada uma, criamos um método calcularPerimetro e adicionamos as seguintes funções:
+    //Para o círculo: 2π para o raio
+    //Para o quadrado: 4 * lado
+//Atenção!
+//Devemos criar um teste para os círculos e um teste para os quadrados.
+//Em seguida, confirme se as duas fórmulas foram calculadas corretamente.
+
+public class Figura {
+}
